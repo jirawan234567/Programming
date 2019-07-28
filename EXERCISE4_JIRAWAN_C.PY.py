1670f520a0fe0127be1edc1c2236ace32a60dca8
jirawan234567 <52350419+jirawan234567@users.noreply.github.com>
2019-07-28 21:18:56 +0700

    Add files via upload

---
+++ EXERCISE4_JIRAWAN_C.PY.py
@@ -0,0 +1,13 @@
+f="Foundation English:"
+FE=80
+g="General Business:"
+GB=70
+i="Introduction to Computer systems:"
+ICS=75
+C="Computer Programming:"
+CR=60.5
+print("-----your Score-----")
+print(f,FE)
+print(g,GB)
+print(i,ICS)
+print(C,CR)
